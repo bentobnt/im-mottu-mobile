256cf87b1f3184ed10267a7a6c4a8b3bbcf5778a
bentobnt <bento.bnt@gmail.com>
2024-09-14 15:42:37 -0300

    Ajuste
    - [App] ajuste na remoção dos dados salvos no fechamento do app

--- marvel_heroes_app/lib/main.dart
+++ marvel_heroes_app/lib/main.dart
@@ -9,6 +9,11 @@ import 'src/presentation/tabbar/tab_bar_page.dart';
 
 Future<void> main() async {
   await runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    final pref = await SharedPreferences.getInstance();
+    pref.clear();
+
     runApp(const MainApp());
   }, (exception, stackTrace) async {
     _catchFlutterExceptions(exception, stackTrace);
@@ -19,44 +24,13 @@ void _catchFlutterExceptions(Object error, StackTrace stack) {
   debugPrintStack(stackTrace: stack, label: error.toString());
 }
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   final String? initialRoute;
 
   const MainApp({
     super.key,
     this.initialRoute,
   });
-
-  @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> with WidgetsBindingObserver {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) async {
-    switch (state) {
-      case AppLifecycleState.detached:
-        final pref = await SharedPreferences.getInstance();
-        pref.clear();
-        break;
-      case AppLifecycleState.paused:
-        print('paused');
-        break;
-      default:
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
